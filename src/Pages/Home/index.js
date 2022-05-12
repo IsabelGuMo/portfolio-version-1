fa79b8485b188388cf5639c65847660490cac4d8
IsabelGuMo <gm.isabel@gmail.com>
2022-05-12 18:52:58 +0200

    Create about me & contact, fix portfolio images, index.css

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -13,11 +13,19 @@ import {
     StickyIn,
     ZoomIn
 } from 'react-scroll-motion';
+
 import './style.css';
+
 import headerImage from '../../img/header-image.jpeg';
 import curriculum from '../../img/curriculum.png'
 import PowerPuffGirls from '../../img/PowerPuffGirls.png'
+import pokeApi from '../../img/pokeApi.png'
+import pokedex from '../../img/pokedex.png'
+import about_me from '../../img/about_me.jpeg'
 
+import { FiMail } from 'react-icons/fi'
+import { FiGithub } from 'react-icons/fi'
+import { TiSocialLinkedin } from 'react-icons/ti'
 
 const Home = () => {
     const ZoomInScrollOut= batch(StickyIn(), FadeIn(), ZoomIn());
@@ -29,17 +37,26 @@ const Home = () => {
             imageSrc: curriculum
         },
         {
-            name: "PowerPuffGirls",
+            name: "PokeApi",
             link: '#',
-            imageSrc: PowerPuffGirls
+            imageSrc: pokeApi
         },
         {
             name: "PowerPuffGirls",
             link: '#',
             imageSrc: PowerPuffGirls
-        }
+        },
+        {
+            name: "Pokédex",
+            link: '#',
+            imageSrc: pokedex
+        },
     ]
 
+const handleContactClick = (url) => {
+    const win = window.open(url, '_blanck')
+    win.focus()
+}
     return(
         
         <ScrollContainer>
@@ -110,9 +127,69 @@ const Home = () => {
                         }
                     </div>
                 </Animator>
-
             </ScrollPage>
 
+         {/* section 3 */}
+            <ScrollPage page={3}>
+                <Animator animation={ZoomInScrollOut}>
+                    <div className="section-title about-me">
+                        About Me
+                    </div>
+                </Animator>
+            </ScrollPage>
+            <ScrollPage page={4}>
+                <Animator animation={batch(Fade(), Sticky(), MoveOut(-1000, -400))}>
+                   <div className="background-image-container">
+                       <img src={about_me} alt="my picture" />
+                       <div className="black-background"></div>
+                   </div> 
+                </Animator>
+                <Animator animation={batch(Fade(), Sticky(), MoveOut(1000, 500))}>
+                    <div className="details">
+                        <h2 className="gretting">Hello</h2>
+                        <p className="bio"> Metus erat varius neque potenti convallis! Dignissim commodo pellentesque euismod, ultricies lorem platea lobortis convallis vulputate eleifend est. Auctor elit rhoncus 
+                        </p>
+                        <p className="bio"> Metus erat varius neque potenti convallis! Dignissim commodo pellentesque euismod, ultricies lorem platea lobortis convallis vulputate eleifend est. 
+                        </p>
+                    </div>
+                </Animator>
+            </ScrollPage>
+
+            {/* section 4 */}
+            <ScrollPage page={5}>
+                <Animator animation={ZoomInScrollOut}>
+                    <div className="section-title contact">
+                        Contact
+                    </div>
+                </Animator>
+            </ScrollPage>
+
+            <ScrollPage page={6}>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100%',
+                }}>
+                    <span style={{ fontSize: '40px'}}>
+
+                    {/* gmail link */}
+                        <Animator animation={MoveIn(1000, 0)}>
+                            <div 
+                                style={{ 
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}
+                                onClick={() => handleContactClick('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox')}
+                                >
+                                <FiMail color='white' className='contact-list-icon' />
+                                    <p>Gmail</p>
+                            </div>
+                        </Animator>
+                    </span>
+                </div>
+            </ScrollPage>
         </ScrollContainer>
         
     )
